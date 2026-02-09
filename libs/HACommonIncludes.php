7cecf3a175f148cfdbc7f58387d55e98f34e969d
bumaas <burkhard.maas@arcor.de>
2026-02-09 12:47:33 +0100

    - HACommonIncludes fehlte im Commit

---
+++ libs/HACommonIncludes.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/HAIds.php';
+require_once __DIR__ . '/HADebug.php';
+require_once __DIR__ . '/HALightDefinitions.php';
+require_once __DIR__ . '/HASwitchDefinitions.php';
+require_once __DIR__ . '/HASensorDefinitions.php';
+require_once __DIR__ . '/HABinarySensorDefinitions.php';
+require_once __DIR__ . '/HAClimateDefinitions.php';
+require_once __DIR__ . '/HANumberDefinitions.php';
+require_once __DIR__ . '/HALockDefinitions.php';
+require_once __DIR__ . '/HACoverDefinitions.php';
+require_once __DIR__ . '/HAEventDefinitions.php';
+require_once __DIR__ . '/HASelectDefinitions.php';
+require_once __DIR__ . '/HAVacuumDefinitions.php';
+require_once __DIR__ . '/HAMediaPlayerDefinitions.php';
