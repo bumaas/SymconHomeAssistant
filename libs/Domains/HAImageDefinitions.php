270734e42285a2a2133bda2b0bfd835775966612
bumaas <burkhard.maas@arcor.de>
2026-04-07 10:09:22 +0200

    1.0 build 44 neu: - Unterstützung für Home-Assistant-image-Entitäten ergänzt - Vorschau-Logik für camera und image vereinheitlicht - image-Status wird als Zeitstempel mit Datum/Uhrzeit dargestellt

---
+++ libs/Domains/HAImageDefinitions.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+final class HAImageDefinitions
+{
+    public const string DOMAIN = 'image';
+    public const int VARIABLE_TYPE = VARIABLETYPE_INTEGER;
+    public const string PRESENTATION = VARIABLE_PRESENTATION_DATE_TIME;
+}
